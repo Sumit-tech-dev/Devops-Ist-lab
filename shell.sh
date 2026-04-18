d1d780691ce42371cd4a022b2845f30f08917b53
Sumit-tech-dev <sumitsachdeva245@gmail.com>
2026-04-18 21:30:40 +0530

    Add positional parameter display to shell script
    Add script to display positional parameters in shell.

--- shell.sh
+++ shell.sh
@@ -47,3 +47,16 @@ ls /var/www/html/
 
 echo "memory status"
 free -m
+#!/bin/bash
+
+echo "Value of 0 is "
+echo $0
+
+echo "Value of 1"
+echo $1
+
+echo "Value of 2"
+echo $2
+
+echo "Value of 3"
+echo $3
